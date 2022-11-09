7bb06acf41a92045e46948e3211394e19444b185
Jay <j.thomastt1@gmail.com>
2022-11-09 00:56:45 -0600

    added routing

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 
 // route files
-//const htmlRoutes = require("./routes/htmlRoutes");
-//const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 
 //intialize express
 const PORT = process.env.port || 3001;
@@ -12,12 +12,12 @@ const PORT = process.env.port || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());  
-//app.use("/api", apiRoutes);
-//app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 
 
 // setup Listener
 app.listen(PORT, () => {
-    console.log(` APP listening on port ${PORT}`)
+    console.log(`APP listening on port ${PORT}`)
 });
